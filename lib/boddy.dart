03af884b18dca80c2fcaf77b990ecec137710b66
nittoranjansarkertanmoy02 <nittaranjansarkertanmoy2022@gmail.com>
2023-04-01 21:40:04 +0600

    day2

--- lib/boddy.dart
+++ lib/boddy.dart
@@ -1,307 +1,45 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, duplicate_ignore, non_constant_identifier_names
-
 import 'package:flutter/material.dart';
+import 'package:quotes_app/constants/images_lists.dart';
+import 'package:quotes_app/constants/text_const.dart';
 
-class Boddy extends StatefulWidget {
-  const Boddy({super.key});
+class Body extends StatefulWidget {
+  const Body({Key? key}) : super(key: key);
 
   @override
-  State<Boddy> createState() => _BoddyState();
+  _BodyState createState() => _BodyState();
 }
 
-class _BoddyState extends State<Boddy> {
-  final Colorchng = Colors.red;
+class _BodyState extends State<Body> {
+  final Color Colorchng = Colors.red;
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Container(
-          child: ListTile(
-            // ignore: prefer_const_constructors
-            leading: CircleAvatar(
-              backgroundImage: AssetImage("images/steve_jobs.jpg"),
-            ),
-            title: Text(
-              "Steve Jobs",
-              style: TextStyle(
-                fontSize: 20,
-                letterSpacing: 2,
+    return Scaffold(
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: perImages.length,
+          itemBuilder: (context, index) {
+            return Card(
+              shape: Border.all(
+                color: Colors.grey,
               ),
-            ),
-            subtitle: Text(
-                "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma which is living with the results of other people's thinking"),
-            trailing: IconButton(
-                onPressed: (() {}),
-                icon: Icon(
-                  Icons.favorite_outline_sharp,
-                )),
-          ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundImage: AssetImage("images/james.webp"),
-            ),
-            title: Text(
-              "James Cameron",
-              style: TextStyle(
-                fontSize: 20,
-                letterSpacing: 2,
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(
+                    perImages[index],
+                  ),
+                ),
+                title: Text(
+                  quotesName[index],
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Text(quotesPersons[index]),
               ),
-            ),
-            subtitle: Text(
-                "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success."),
-            trailing: IconButton(
-                onPressed: (() {}),
-                icon: Icon(
-                  Icons.favorite_outline_sharp,
-                )),
-          ),
+            );
+          },
         ),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundImage: AssetImage("images/nelson.jpg"),
-            ),
-            title: Text(
-              "Nelson Mandela",
-              style: TextStyle(
-                fontSize: 20,
-                letterSpacing: 2,
-              ),
-            ),
-            subtitle: Text(
-                "The greatest glory in living lies not in never falling, but in rising every time we fall."),
-            trailing: IconButton(
-                onPressed: (() {}),
-                icon: Icon(
-                  Icons.favorite_outline_sharp,
-                )),
-          ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundImage: AssetImage("images/aristotle.jpg"),
-            ),
-            title: Text(
-              "Aristotle",
-              style: TextStyle(
-                fontSize: 20,
-                letterSpacing: 2,
-              ),
-            ),
-            subtitle: Text(
-                "It is during our darkest moments that we must focus to see the light."),
-            trailing: IconButton(
-                onPressed: (() {}),
-                icon: Icon(
-                  Icons.favorite_outline_sharp,
-                )),
-          ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundImage: AssetImage('images/waldo.jpg'),
-            ),
-            title: Text(
-              "Ralph Waldo Emerson",
-              style: TextStyle(
-                fontSize: 20,
-                letterSpacing: 2,
-              ),
-            ),
-            subtitle: Text(
-                "Do not go where the path may lead, go instead where there is no path and leave a trail."),
-            trailing: IconButton(
-                onPressed: (() {}),
-                icon: Icon(
-                  Icons.favorite_outline_sharp,
-                )),
-          ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundImage: AssetImage('images/teresa.jpg'),
-            ),
-            title: Text(
-              "Mother Teresa",
-              style: TextStyle(
-                fontSize: 20,
-                letterSpacing: 2,
-              ),
-            ),
-            subtitle: Text(
-                "Spread love everywhere you go.Let no one ever come to you without leaving happier."),
-            trailing: IconButton(
-                onPressed: (() {}),
-                icon: Icon(
-                  Icons.favorite_outline_sharp,
-                )),
-          ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundImage: AssetImage("images/Anne.webp"),
-            ),
-            title: Text(
-              "Anne Frank",
-              style: TextStyle(
-                fontSize: 20,
-                letterSpacing: 2,
-              ),
-            ),
-            subtitle: Text("Whoever is happy will make others happy too."),
-            trailing: IconButton(
-                onPressed: (() {}),
-                icon: Icon(
-                  Icons.favorite_outline_sharp,
-                )),
-          ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundImage: AssetImage("images/abraham.webp"),
-            ),
-            title: Text(
-              "Abraham Lincoln",
-              style: TextStyle(
-                fontSize: 20,
-                letterSpacing: 2,
-              ),
-            ),
-            subtitle: Text(
-                "In the end, it's not the years in your life that count. It's the life in your years."),
-            trailing: IconButton(
-                onPressed: (() {}),
-                icon: Icon(
-                  Icons.favorite_outline_sharp,
-                )),
-          ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundImage: AssetImage('images/Einstein.jpg'),
-            ),
-            title: Text(
-              "Albert Einstein",
-              style: TextStyle(
-                fontSize: 20,
-                letterSpacing: 2,
-              ),
-            ),
-            subtitle:
-                Text("The only mistake in life is the lesson not learned."),
-            trailing: IconButton(
-                onPressed: (() {}),
-                icon: Icon(
-                  Icons.favorite_outline_sharp,
-                )),
-          ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundImage: AssetImage('images/maya.webp'),
-            ),
-            title: Text(
-              "Maya Angelou",
-              style: TextStyle(
-                fontSize: 20,
-                letterSpacing: 2,
-              ),
-            ),
-            subtitle: Text(
-                "I’ve learned that no matter what happens, or how bad it seems today, life does go on, and it will be better tomorrow."),
-            trailing: IconButton(
-                onPressed: (() {}),
-                icon: Icon(
-                  Icons.favorite_outline_sharp,
-                )),
-          ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundImage: AssetImage("images/tim.jpg"),
-            ),
-            title: Text(
-              "Tim Rice",
-              style: TextStyle(
-                fontSize: 20,
-                letterSpacing: 2,
-              ),
-            ),
-            subtitle: Text("The biggest lesson I had in my life was failure"),
-            trailing: IconButton(
-                onPressed: (() {}),
-                icon: Icon(
-                  Icons.favorite_outline_sharp,
-                )),
-          ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundImage: AssetImage('images/mary.webp'),
-            ),
-            title: Text(
-              "Mary Engelbreit",
-              style: TextStyle(
-                fontSize: 20,
-                letterSpacing: 2,
-              ),
-            ),
-            subtitle: Text("Life’s too mysterious to take too serious."),
-            trailing: IconButton(
-                onPressed: (() {}),
-                icon: Icon(
-                  Icons.favorite_outline_sharp,
-                )),
-          ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-      ],
+      ),
     );
   }
 }
--- lib/constants/text_const.dart
+++ lib/constants/text_const.dart
@@ -1,3 +1,68 @@
 //Splash Screen====================================
 const nextPage = "Next";
 const skipPage = "Skip";
+
+//======================Quotes=============Names====================
+const stevjobs = "Steve Jobs";
+const james = "James Cameron";
+const nelson = "Nelson Mandela";
+const aristole = "Aristotle";
+const ralph = "Ralph Waldo Emerson";
+const teresa = "Mother Teresa";
+const anne = "Anne Frank";
+const abraham = "Abraham Lincoln";
+const alber = "Albert Einstein";
+const maya = "Maya Angelou";
+const tim = "Tim Rice";
+const mary = "Mary Engelbrei";
+
+const quotesName = [
+  stevjobs,
+  james,
+  nelson,
+  aristole,
+  ralph,
+  teresa,
+  anne,
+  abraham,
+  alber,
+  maya,
+  tim,
+  mary
+];
+
+//=====================Quotes============================
+
+const steveJobs =
+    "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma which is living with the results of other people's thinking";
+const jamesCameron =
+    "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.";
+const nelsonMandela =
+    "The greatest glory in living lies not in never falling, but in rising every time we fall.";
+const aristol =
+    "It is during our darkest moments that we must focus to see the light.";
+const ralphh =
+    "Do not go where the path may lead, go instead where there is no path and leave a trail.";
+const tereesa =
+    "Spread love everywhere you go.Let no one ever come to you without leaving happier.";
+const annee = "Whoever is happy will make others happy too.";
+const abrahamLinkon =
+    "In the end, it's not the years in your life that count. It's the life in your years.";
+const albertEinstein = "The only mistake in life is the lesson not learned.";
+const mayaAngelou =
+    "I’ve learned that no matter what happens, or how bad it seems today, life does go on, and it will be better tomorrow.";
+const timRice = "The biggest lesson I had in my life was failure.";
+const maryEngelbrei = "Life’s too mysterious to take too serious.";
+
+const quotesPersons = [
+  steveJobs,
+  jamesCameron,
+  aristol,
+  ralphh,
+  tereesa,
+  annee,
+  albertEinstein,
+  mayaAngelou,
+  timRice,
+  maryEngelbrei
+];
