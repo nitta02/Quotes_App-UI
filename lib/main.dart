6fb8e9bebd43c3d0f3e0f92a8f1858e5c6f2c446
nittoranjansarkertanmoy02 <nittaranjansarkertanmoy2022@gmail.com>
2023-07-06 09:56:28 +0600

    day12.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:quotes_app/firebase_options.dart';
 import 'package:quotes_app/screen/splash_screen/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: SplashScreen(),
+      home: const SplashScreen(),
     );
   }
 }
