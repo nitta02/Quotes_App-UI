2fa84940b7e7fa7cffdfdcc42069639e4e038757
nittoranjansarkertanmoy02 <nittaranjansarkertanmoy2022@gmail.com>
2023-07-06 10:43:37 +0600

    day12.2

--- lib/screen/screens/login_screen.dart
+++ lib/screen/screens/login_screen.dart
@@ -28,144 +28,174 @@ class _loginScreenState extends State<loginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("images/bgimg2.png"),
-              fit: BoxFit.fill,
-              opacity: 0.91,
-              colorFilter: ColorFilter.mode(Colors.black54, BlendMode.darken)),
-        ),
-        child: Form(
-            key: _formKey,
-            child: Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 60,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.0),
-                        border: Border.all(
-                          width: 2,
-                          color: Colors.grey,
-                        )),
-                    child: TextFormField(
-                      controller: emailController,
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          prefixIcon: Icon(
-                            CupertinoIcons.mail_solid,
-                          ),
-                          prefixIconColor: Colors.white,
-                          hintText: "Enter Your E-mail",
-                          hintStyle: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w400,
-                            letterSpacing: 1.8,
-                          )),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Kindly Enter E-mail";
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  25.heightBox,
-                  Container(
-                    height: 60,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey,
-                        width: 2,
-                      ),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: TextFormField(
-                      controller: passwordController,
-                      obscureText: true,
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(
-                          prefixIcon: Icon(CupertinoIcons.lock_open),
-                          prefixIconColor: Colors.white,
-                          border: InputBorder.none,
-                          hintText: "Enter Your Password",
-                          hintStyle: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w400,
-                            letterSpacing: 1.8,
-                          )),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Kindly Enter Password";
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  50.heightBox,
-                  OutlinedButton(
-                      onLongPress: () {},
-                      style: ButtonStyle(
-                          minimumSize: MaterialStatePropertyAll(Size(450, 45)),
-                          backgroundColor: MaterialStatePropertyAll(
-                              Color.fromARGB(255, 101, 98, 98))),
-                      onPressed: () async {
-                        SharedPreferences sp =
-                            await SharedPreferences.getInstance();
-                        sp.setString('email', emailController.text.toString());
-                        sp.setBool('isLogin', true);
-
-                        setState(() {
-                          if (_formKey.currentState!.validate()) {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => HomeScreen(),
-                                ));
-                          }
-                        });
-                      },
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 1.8,
-                          color: Colors.white,
-                        ),
-                      )),
-                  15.heightBox,
-                  Row(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Container(
+              //   height: 30,
+              //   width: 30,
+              //   child: Image.asset('assets/icons/ideaIcon.png'),
+              // ),
+              50.heightBox,
+              Form(
+                  key: _formKey,
+                  child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      "Don't have any account ?"
-                          .text
-                          .letterSpacing(1.8)
-                          .color(Colors.white)
-                          .make(),
-                      TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => SignupPage(),
-                                ));
+                      Container(
+                        height: 60,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5.0),
+                            border: Border.all(
+                              width: 2,
+                            )),
+                        child: Center(
+                          child: TextFormField(
+                            controller: emailController,
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                prefixIcon: Icon(
+                                  CupertinoIcons.mail_solid,
+                                ),
+                                hintText: "Enter Your E-mail",
+                                hintStyle: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  letterSpacing: 1.8,
+                                )),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Kindly Enter E-mail";
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                      ),
+                      25.heightBox,
+                      Container(
+                        height: 60,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                        child: Center(
+                          child: TextFormField(
+                            controller: passwordController,
+                            obscureText: true,
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                                prefixIcon: Icon(CupertinoIcons.lock_open),
+                                border: InputBorder.none,
+                                hintText: "Enter Your Password",
+                                hintStyle: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  letterSpacing: 1.8,
+                                )),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Kindly Enter Password";
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                      ),
+                      40.heightBox,
+                      OutlinedButton(
+                          onLongPress: () {},
+                          style: ButtonStyle(
+                              minimumSize:
+                                  MaterialStatePropertyAll(Size(450, 45)),
+                              backgroundColor: MaterialStatePropertyAll(
+                                  Color.fromARGB(255, 101, 98, 98))),
+                          onPressed: () async {
+                            SharedPreferences sp =
+                                await SharedPreferences.getInstance();
+                            sp.setString(
+                                'email', emailController.text.toString());
+                            sp.setBool('isLogin', true);
+
+                            setState(() {
+                              if (_formKey.currentState!.validate()) {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => HomeScreen(),
+                                    ));
+                              }
+                            });
                           },
                           child: Text(
-                            "Sign up",
+                            "Login",
                             style: TextStyle(
-                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
                               letterSpacing: 1.8,
+                              color: Colors.white,
                             ),
-                          ))
+                          )),
+                      15.heightBox,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          "Don't have any account ?"
+                              .text
+                              .letterSpacing(1.8)
+                              .make(),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => SignupPage(),
+                                    ));
+                              },
+                              child: Text(
+                                "Sign up",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  letterSpacing: 1.8,
+                                ),
+                              ))
+                        ],
+                      ),
+                      8.heightBox,
+                      Text('Or'),
+                      15.heightBox,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Sign in with',
+                            style: TextStyle(
+                              letterSpacing: 1.8,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          10.widthBox,
+                          GestureDetector(
+                            onTap: () {
+                              print('Tapped');
+                            },
+                            child: Container(
+                              height: 30,
+                              width: 30,
+                              child: Image.asset('assets/icons/google.png'),
+                            ),
+                          )
+                        ],
+                      ),
                     ],
-                  )
-                ],
-              ),
-            )),
+                  )),
+            ],
+          ),
+        ),
       ),
     );
   }
