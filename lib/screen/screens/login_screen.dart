9ed4cd743fbbc5fe4bcc0458e653cf36791c7241
nittoranjansarkertanmoy02 <nittaranjansarkertanmoy2022@gmail.com>
2023-07-06 18:41:42 +0600

    12.2

--- lib/screen/screens/login_screen.dart
+++ lib/screen/screens/login_screen.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors, camel_case_types, prefer_const_literals_to_create_immutables, avoid_print, unrelated_type_equality_checks, use_build_context_synchronously
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:quotes_app/screen/home_screen.dart';
 import 'package:quotes_app/screen/screens/signup_screen.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -23,6 +25,34 @@ class _loginScreenState extends State<loginScreen> {
     super.dispose();
     emailController.dispose();
     passwordController.dispose();
+  }
+
+  _signingoogle() {
+    _signInWithGoogle().then((value) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomeScreen(),
+          ));
+    });
+  }
+
+  Future<UserCredential> _signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
   @override
@@ -68,7 +98,7 @@ class _loginScreenState extends State<loginScreen> {
                                 )),
                             validator: (value) {
                               if (value == null || value.isEmpty) {
-                                return "Kindly Enter E-mail";
+                                return 'Kindly Enter E-mail';
                               }
                               return null;
                             },
@@ -181,7 +211,7 @@ class _loginScreenState extends State<loginScreen> {
                           10.widthBox,
                           GestureDetector(
                             onTap: () {
-                              print('Tapped');
+                              _signingoogle();
                             },
                             child: Container(
                               height: 30,
